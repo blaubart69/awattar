5a4d95086955a3e25a1a6c49a71944b980d5f078
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2023-08-11 18:20:04 +0200

    added EuroEarned. all hours with prices below zero summed

--- 20_calc_month.ps1
+++ 20_calc_month.ps1
@@ -3,20 +3,39 @@ param (
     ,[int]$month
 )
 
+# Date      : 31.07.2023
+# Hour      : 14
+# Timestamp : 7/31/2023 2:00:00PM
+# baseprice : 6.01
+# CentkWh   : 6.01
+# Centx100  : 601
+# kWh       : 0.197
+# kWhx1000  : 197
+# PriceCent : 1.18397
+$joinedData = (& "$PSScriptRoot/Join-Price_kWh.ps1" -year $year -month $month)
+
 $groupPerDay = 
 (
-    & "$PSScriptRoot/Join-Price_kWh.ps1" -year $year -month $month
+      $joinedData
     | Group-Object -Property Date
     | % {
         $EuroNet = ( $_.Group | Measure-Object -Sum PriceCent ).Sum / 100
         [PSCustomObject]@{
             Date       = $_.Name
             EuroNet    = $EuroNet
-            EuroBrutto = $EuroNet * 1.2
+            #EuroBrutto = $EuroNet * 1.2
             kWhDay     = ( $_.Group | Measure-Object -Sum kWh ).Sum
         }
     }
 )
+
+$CentMinus = 0
+
+$joinedData | % { 
+    if ( $_.Centx100 -lt 0 ) {
+        $CentMinus += -$_.PriceCent
+    }
+}
 
 $groupPerDay
 
@@ -34,6 +53,7 @@ $kWhAverage  = $kwHSum / $daysInMonth
     Awattar     = $awatt
     EuroBrutto  = $SumNet + $vat
     EuroTotal   = $SumNet + $vat + $awatt
+    EuroEarned  = $CentMinus / 100
     kWhSum      = $kwHSum
     kWhAveragePerDay = $kWhAverage
     CentPerKwH = $SumNet / $kwHSum * 100
